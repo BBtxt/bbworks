192a5bde496ad975e3948279843759d9cd5b58ba
Brandon Baker <bbaker.sh@gmail.com>
2023-03-25 11:05:23 -0700

    added navbar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,68 +2,113 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
+import menu_icon from "../../public/menu-vertical-svgrepo-com.svg";
 
 const Page = ({ page, selectedPage, setSelectedPage }) => {
-    const lowerCasePage = page.toLowerCase();
-    return (
-        <AnchorLink
-        className={`${selectedPage === lowerCasePage ? "text-violet-400" : "" }
-        hover:text-violet-400 transition duration-500`}
-        href={`#${lowerCasePage}`}
-        onClick={() => setSelectedPage(lowerCasePage)}>
-            {page}
-        </AnchorLink>
-    )
-}
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-violet-400" : ""}
+        hover:text-violet-400 transition duration-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const isAboveSmallScreen = useMediaQuery("(min-width: 768px)");
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isAboveSmallScreen = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-violet-300 transition duration-200";
 
-    return(
-        <nav className={`z-40 w-full fixed top-0 py-6`}>
-            <div className="flex items-center justify-between mx-auto w-5/6">
-                <h4 className="text-8xl font-bold">BB</h4>
+  return (
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="text-8xl font-bold">BB</h4>
 
-                {/* Desktop Nav */}
-                {isAboveSmallScreen ? (
-                    <div className="flex justify-between gap-16 text-sm font-semibold">
-                        <Page
-                        page='Home'
-                        selectedPage={selectedPage}
-                        setSelectedPage={selectedPage} />
-                        <Page
-                        page='About'
-                        selectedPage={selectedPage}
-                        setSelectedPage={selectedPage} />
-                        <Page
-                        page='Projects'
-                        selectedPage={selectedPage}
-                        setSelectedPage={selectedPage} />
-                        <Page
-                        page='Skills'
-                        selectedPage={selectedPage}
-                        setSelectedPage={selectedPage} />
-                        <Page
-                        page='Contact'
-                        selectedPage={selectedPage}
-                        setSelectedPage={selectedPage} />
-                    </div>
-                ):(
-                    <button 
-                    className="rounded-full p-2">
-                        <Image 
-                        alt="menu" 
-                        src="../asset/menu-icon.svg"
-                        className="w-8 h-8"
-                        width={36}
-                        height={36} />
-                    </button>
-                )}
+        {/* Desktop Nav */}
+        {isAboveSmallScreen ? (
+          <div className="flex justify-between gap-16 text-sm font-semibold">
+            <Page
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={selectedPage}
+            />
+            <Page
+              page="About"
+              selectedPage={selectedPage}
+              setSelectedPage={selectedPage}
+            />
+            <Page
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={selectedPage}
+            />
+            <Page
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={selectedPage}
+            />
+            <Page
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={selectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full p-2"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <Image alt="menu" src={menu_icon} width={36} height={36} />
+          </button>
+        )}
+
+        {/* Mobile Nav */}
+        {!isAboveSmallScreen && isMenuOpen && (
+          <div className="fixed right-0 bottom-0 h-full w-[300px] bg-white border-dotted border-2 border-zinc-400">
+            {/* Closed Mobile Nav */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <Image alt="menu" src={menu_icon} width={36} height={36} />
+              </button>
             </div>
 
-        </nav>
-    )
-}
+            {/* Menu Items */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl">
+              <Page
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={selectedPage}
+              />
+              <Page
+                page="About"
+                selectedPage={selectedPage}
+                setSelectedPage={selectedPage}
+              />
+              <Page
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={selectedPage}
+              />
+              <Page
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={selectedPage}
+              />
+              <Page
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={selectedPage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
