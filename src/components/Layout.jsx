d3187d175e22d1985e42eed99dd71f760b3de8a4
Brandon Baker <bbaker.sh@gmail.com>
2023-03-25 11:05:59 -0700

    added navbar and scroll funtionality

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Navbar from "@/components/Navbar";
 
 const Layout = () => {
   const [selectedPage, setSelectedPage] = useState("home");
-  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)")
-  return (
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
 
-  <div className=" app ">
-    <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
-  </div>
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className=" app ">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreen && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default Layout;
