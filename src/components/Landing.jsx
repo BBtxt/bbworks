2278219dfc5d896bafd8635564ad16625eaaf109
Brandon Baker <bbaker.sh@gmail.com>
2023-03-29 21:26:13 -0700

    cleaned up some of the ui

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -10,12 +10,12 @@ const Landing = ({ setSelectedPage }) => {
   return (
     <section
       id="home"
-      className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10 mt-20"
+      className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10 mt-32 mb-32"
     >
       {/* Image Section */}
       <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
         {isAboveMediumScreen ? (
-          <div className="relative z-0 before:absolute before:-top-10 before:-left-10 before:w-full before:h-full before:border-2 before:border-violet-400 before:z-[-1]">
+          <div className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10 before:w-full before:h-full before:border-2 before:border-violet-400 before:z-[-1]">
             <Image
               className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
               src="/ProfilePic.jpeg"
@@ -38,7 +38,7 @@ const Landing = ({ setSelectedPage }) => {
         )}
       </div>
       {/* Main TEXT TING  */}
-      <div className="z-30 basis mt-12 md:mt32 ">
+      <div className="z-30 basis-2/5 mt-12 md:mt32 ">
         {/* Heading */}
         <motion.div
           initial="hidden"
@@ -100,9 +100,9 @@ const Landing = ({ setSelectedPage }) => {
           variants={{
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
-          }}>
-            
-            < SocialMediaIcons />
+          }}
+        >
+          <SocialMediaIcons />
         </motion.div>
       </div>
     </section>
