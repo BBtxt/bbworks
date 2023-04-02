53df3f5ca5752b4d0135bea3b6515fa3513e0942
Brandon Baker <bbaker.sh@gmail.com>
2023-04-01 17:32:44 -0700

    created new comp for project page

---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import useMediaQuery from '@/hooks/useMediaQuery';
+
+const Projects = () => {
+    const isAboveMediumScreen = useMediaQuery("(min-width: 769px)");
+  return (
+<section id="projects" className="pt-10 pb-24">
+
+</section>  
+)
+}
+
+export default Projects
