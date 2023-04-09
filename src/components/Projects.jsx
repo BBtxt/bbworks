ae2288fabc89333df69bc5664162a678f71cba38
Brandon Baker <bbaker.sh@gmail.com>
2023-04-08 21:38:04 -0700

    added second project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const Projects = () => {
 
       {/* PROJECT 1 */}
 
-      <div className="md:w4/5 mx-auto content-center">
+      <div className="md:w4/5 mx-auto content-center mb-12">
         <p className="text-2xl font-semibold text-center mb-4">/bblog_world</p>
         <div className="flex justify-center flex-col gap-12 md:flex-row  md:justify-stretch">
           <div className="">
@@ -145,6 +145,111 @@ const Projects = () => {
                   <a
                     className="text-sm  underline"
                     href="https://github.com/BBtxt/bblog_world"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Code
+                  </a>
+                </motion.li>
+              </motion.ul>
+            </motion.nav>
+          </div>
+        </div>
+      </div>
+
+      {/* PROJECT 2 */}
+      <div className="md:w4/5 mx-auto content-center">
+        <p className="text-2xl font-semibold text-center mb-4">/threeD</p>
+        <div className="flex justify-center flex-col gap-12 md:flex-row  md:justify-stretch">
+          <div className="">
+            <Image
+              className="mx-auto"
+              src="/three3D.gif"
+              alt="3d website gif"
+              width={600}
+              height={600}
+            />
+          </div>
+          <div className="object-contain w-full md:w-1/2">
+            <motion.nav
+              className=""
+              initial={false}
+              animate={isOpen ? "open" : "closed"}
+            >
+              <motion.button
+                className=" w-full flex items-center justify-between gap-6 border p-2 mb-4"
+                whileTap={{ scale: 0.97 }}
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                About
+                <motion.div
+                  variants={{
+                    open: { rotate: 180 },
+                    closed: { rotate: 0 },
+                  }}
+                  transition={{ duration: 0.2 }}
+                  style={{ originY: 0.55 }}
+                >
+                  <svg width="15" height="15" viewBox="0 0 20 20">
+                    <path d="M0 7 L 20 7 L 10 16" />
+                  </svg>
+                </motion.div>
+              </motion.button>
+              <motion.ul
+                className="border-dotted border-2 border-black p-4"
+                variants={{
+                  open: {
+                    clipPath: "inset(0% 0% 0% 0% round 10px)",
+                    transition: {
+                      type: "spring",
+                      bounce: 0,
+                      duration: 0.7,
+                      delayChildren: 0.3,
+                      staggerChildren: 0.05,
+                    },
+                  },
+                  closed: {
+                    clipPath: "inset(10% 50% 90% 50% round 10px)",
+                    transition: {
+                      type: "spring",
+                      bounce: 0,
+                      duration: 0.3,
+                    },
+                  },
+                }}
+                style={{ pointerEvents: isOpen ? "auto" : "none" }}
+              >
+                <motion.li
+                  variants={itemVariants}
+                  className="text-sm font-semibold"
+                >
+                  Abstract:
+                </motion.li>
+                <motion.li variants={itemVariants} className="text-sm">
+                  Application that takes of Three.js library to create a 3D space scene that can be navigated with the mouse. Includes randomly generated stars and planet with rotating ring. 
+                </motion.li>
+                <motion.li
+                  className="text-sm font-semibold"
+                  variants={itemVariants}
+                >
+                  Status:
+                </motion.li>
+                <motion.li
+                  variants={itemVariants}
+                  className="text-sm font-semibold text-red-400 "
+                >
+                  Out Development
+                </motion.li>
+                <motion.li
+                  variants={itemVariants}
+                  className="text-sm font-semibold"
+                >
+                  Links:
+                </motion.li>
+                <motion.li variants={itemVariants}>
+                  <a
+                    className="text-sm  underline"
+                    href="https://github.com/BBtxt/ThreeD"
                     target="_blank"
                     rel="noreferrer"
                   >
